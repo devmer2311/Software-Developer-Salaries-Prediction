7e21b4fd5c1bbcde16649f5a17e9ec5226a89486
devmer2311 <138443145+devmer2311@users.noreply.github.com>
2024-08-01 18:14:10 +0530

    Update predictnew.py

--- predictnew.py
+++ predictnew.py
@@ -99,6 +99,8 @@ def get_user_input(user_age, user_edlevel, user_country, user_workexp, user_skil
     user_df = pd.DataFrame([user_features])
     return user_df
 
+if 'show_about' not in st.session_state:
+    st.session_state.show_about = False
 
 # Header with logo and navbar using HTML and CSS
 header_html = """
